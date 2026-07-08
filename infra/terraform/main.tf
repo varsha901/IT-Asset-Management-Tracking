596426a97464d9aa47d7e7b60a6878808433af7d
Varsha Murali <varshakodavati23@gmail.com>
2026-07-07 23:13:42 -0400

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "app" {
