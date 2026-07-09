42e6c7767b0b5ef3e9c49788083fbc040be1b725
Varsha Murali <varshakodavati23@gmail.com>
2026-07-08 22:18:50 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/assettrack');
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/assettrack', {
+  serverSelectionTimeoutMS: 10000
+  });
 };
 
 module.exports = { connectDB };
